0b30fb66aa5649423bee8bc03dea65213c8f2f5a
Mayur :) <md@pooraa.com>
2019-09-15 21:13:17 +0530

    struct fields set function added

---
+++ favour/favour.go
@@ -0,0 +1,91 @@
+package favour
+
+import (
+	"errors"
+	"reflect"
+	"time"
+)
+
+//struct tags
+const (
+	SQLTag     = "sql"
+	JSONTag    = "json"
+	GORMTag    = "gorm"
+	DefaultTag = "default"
+	Null       = "null"
+)
+
+//MergeMap will merge two maps
+func MergeMap(a, b map[string]interface{}) {
+	for k, v := range b {
+		a[k] = v
+	}
+}
+
+//GetAVal will return first non empty string value
+func GetAVal(val ...string) string {
+	for _, v := range val {
+		if v != "" {
+			return v
+		}
+	}
+	return ""
+}
+
+//GetIVal will return first non empty int value
+func GetIVal(val ...int) int {
+	for _, v := range val {
+		if v != 0 {
+			return v
+		}
+	}
+	return 0
+}
+
+//GetFVal will return first non empty float64 value
+func GetFVal(val ...float64) float64 {
+	for _, v := range val {
+		if v != 0 {
+			return v
+		}
+	}
+	return 0
+}
+
+//GetFieldVal Convert reflect value to its corrosponding data type
+func GetFieldVal(val reflect.Value) (castValue interface{}, err error) {
+	switch val.Kind() {
+	case reflect.String:
+		castValue = val.String()
+	case reflect.Int, reflect.Int16, reflect.Int32, reflect.Int64:
+		castValue = val.Int()
+	case reflect.Float32, reflect.Float64:
+		castValue = val.Float()
+	case reflect.Map, reflect.Slice, reflect.Struct, reflect.Interface:
+		castValue = val.Interface()
+	default:
+		err = errors.New("GetFieldVal: Invalid Filed Kind")
+	}
+	return
+}
+
+//IsDefault Check whether value is default
+func IsDefault(val reflect.Value) (isDefault bool) {
+	if val.IsNil() || !val.IsValid() {
+		isDefault = true
+	} else {
+		zero := reflect.Zero(val.Type())
+		fKind := val.Kind()
+
+		if tVal, ok := val.Interface().(time.Time); ok {
+			isDefault = tVal.IsZero()
+		} else if fKind == reflect.Map || fKind == reflect.Slice {
+			if val.Len() == 0 {
+				isDefault = true
+			}
+		} else if val.Interface() == zero.Interface() {
+			isDefault = reflect.DeepEqual(val, zero)
+		}
+	}
+	return
+}
---
+++ favour/struct.go
@@ -0,0 +1,82 @@
+package favour
+
+import (
+	"errors"
+	"reflect"
+	"strings"
+)
+
+//StructTagValue will return struct model tag field and value from specific tag
+func StructTagValue(input interface{}, tag string) (fields map[string]interface{}, err error) {
+
+	var refObj reflect.Value
+	tag = GetAVal(tag, SQLTag)
+	fields = make(map[string]interface{})
+	refObj, err = getStructRefObj(input)
+
+	if err == nil {
+		for i := 0; i < refObj.NumField(); i++ {
+			refField := refObj.Field(i)
+			refType := refObj.Type().Field(i)
+
+			//if not exported
+			if refType.Name[0] > 'Z' {
+				continue
+			}
+
+			//checking embeded anonymous structures
+			if refType.Anonymous && refField.Kind() == reflect.Struct {
+				var embdFields map[string]interface{}
+				if embdFields, err = StructTagValue(refField.Interface(), tag); err != nil {
+					break
+				}
+				MergeMap(fields, embdFields)
+			} else {
+				if err = setFiledVal(tag, refType, refField, fields); err != nil {
+					break
+				}
+			}
+		}
+	}
+	return
+}
+
+//getRefObj will return struct reflect obj
+func getStructRefObj(input interface{}) (refObj reflect.Value, err error) {
+	refObj = reflect.ValueOf(input)
+	if refObj.Kind() == reflect.Ptr {
+		refObj = refObj.Elem()
+	}
+	if refObj.Kind() != reflect.Struct || !refObj.IsValid() {
+		err = errors.New("No a struct")
+	}
+	return
+}
+
+//setFiledVal will set struct current field value
+func setFiledVal(tag string, refType reflect.StructField, refField reflect.Value,
+	fields map[string]interface{}) (err error) {
+	if col, exists := refType.Tag.Lookup(tag); exists {
+		isDef := IsDefault(refField)
+		if col == "-" ||
+			(strings.Contains(col, ",omitempty") && isDef) {
+			return
+		}
+		if tag == GORMTag {
+			if sqlCol := refType.Tag.Get(SQLTag); sqlCol == "-" {
+				return
+			}
+			col = strings.TrimPrefix(col, "column:")
+		}
+		col = strings.Split(col, ",")[0]
+		dVal := refType.Tag.Get(DefaultTag)
+
+		//if default tag set to null
+		if strings.ToLower(dVal) == Null && isDef {
+			fields[col] = nil
+		} else {
+			fields[col], err = GetFieldVal(refField)
+		}
+	}
+	return
+}
