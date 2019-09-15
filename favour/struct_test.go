e7c1ecf85214cc96cdd1dc9e2c6a97e2d1a87bdc
Mayur :) <md@pooraa.com>
2019-09-15 21:33:58 +0530

    embeded struct test added

--- favour/struct_test.go
+++ favour/struct_test.go
@@ -9,17 +9,60 @@ import (
 func TestStructTag(t *testing.T) {
 	a := assert.New(t)
 
-	type basic struct {
+	type basicJSON struct {
 		Name   string  `json:"name"`
 		Age    float64 `json:"age"`
 		Number int     `json:"number"`
+		Value  int     `sql:"value,omitempty"`
 	}
 
-	b := basic{Name: "Mayur", Age: 24, Number: 123123}
+	bJSON := basicJSON{Name: "Mayur", Age: 24, Number: 123123}
 
-	v, err := StructTagValue(b, "")
+	v, err := StructTagValue(bJSON, JSONTag)
 	a.NoError(err)
 	a.Equal("Mayur", v["name"])
 	a.Equal(24.0, v["age"])
 	a.Equal(int64(123123), v["number"])
+	a.Nil(v["value"])
+
+	type basicSQL struct {
+		Name   string  `sql:"name"`
+		Age    float64 `sql:"age,omitempty"`
+		Number int     `sql:"number"`
+		Ignore string  `sql:"-"`
+	}
+
+	bSQL := basicSQL{Name: "Mayur", Age: 24, Number: 123123, Ignore: "Ignore"}
+
+	v, err = StructTagValue(bSQL, SQLTag)
+	a.NoError(err)
+	a.Equal("Mayur", v["name"])
+	a.Equal(24.0, v["age"])
+	a.Equal(int64(123123), v["number"])
+	a.Nil(v["ignore"])
+}
+
+func TestEmbededStruct(t *testing.T) {
+	a := assert.New(t)
+
+	type Common struct {
+		Number int `json:"number"`
+		Value  int `sql:"value,omitempty"`
+	}
+
+	type basicJSON struct {
+		Name string  `json:"name"`
+		Age  float64 `json:"age"`
+		Common
+	}
+
+	bJSON := basicJSON{Name: "Mayur", Age: 24}
+	bJSON.Number = 123123
+
+	v, err := StructTagValue(bJSON, JSONTag)
+	a.NoError(err)
+	a.Equal("Mayur", v["name"])
+	a.Equal(24.0, v["age"])
+	a.Equal(int64(123123), v["number"])
+	a.Nil(v["value"])
 }
